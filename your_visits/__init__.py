ff673241560d2a17496b0aeb8d0271b59edcd8f3
atHome <cloudonly21@gmail.com>
2021-11-26 12:12:52 +0900

    use flask-migrate

--- your_visits/__init__.py
+++ your_visits/__init__.py
@@ -1,29 +1,26 @@
 import os
 
-from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
 def create_app():
     app = Flask(__name__)
-    load_dotenv()
 
     # DB
-    # in-memory. see https://docs.sqlalchemy.org/en/14/core/engines.html
-    # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://"
-    # uncomment below for saving to a file
     app.config[
         "SQLALCHEMY_DATABASE_URI"
     ] = f"sqlite:///{os.path.join(BASE_DIR, 'dev.db')}"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    from .models import db
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
     # Views:
     from .routes import bp
